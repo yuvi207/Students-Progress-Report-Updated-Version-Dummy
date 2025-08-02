8dc969115df46b15450b21c3be8b6f9696bac2d0
Yuvaraj <bmsyuvan@gmail.com>
2025-08-02 14:33:28 +0530

    Updated Version 9.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { students } from "./studentsdata";
+import { students } from "./STD11studentsdata";
 import "./App.css";
 function App() {
   const [studentClass, setStudentClass] = useState("");
